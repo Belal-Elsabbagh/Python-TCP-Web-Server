7dca44d33d6fdfd3ae1ee5f5528d7dd8c5cd2c94
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-06-01 23:05:31 +0200

    Added parameter to send_file that takes a string to send http response

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from socket import *
 
 
-def send_file(conn_socket: socket, data_str: str) -> None:
+def send_file(conn_socket: socket, http_response: str, data_str: str) -> None:
     """
     Sends a string through the connection socket
+    :param http_response: HTTP response message
     :rtype: None
     :param conn_socket: The open connection socket
     :param data_str: The string 
     """
-    conn_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+    conn_socket.send(http_response.encode())
     conn_socket.send(data_str.encode())
     connectionSocket.send("\r\n".encode('UTF-8'))
 
 
+HOST_IP: str = "127.0.0.1"
 TCP_PORT: int = 6789
 BUFFER_SIZE: int = 4096
-HOST_IP: str = "127.0.0.1"
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((HOST_IP, TCP_PORT))
@@ -28,13 +29,19 @@ while True:
     print('Connection address:', addr)
     try:
         message = connectionSocket.recv(BUFFER_SIZE)
+        print("HTTP Request: " + message.decode('utf-8'))
         filename = message.split()[1]
+
         requestedFile = open(filename[1:], 'r')
         requestedFileData = requestedFile.read()
-        send_file(connectionSocket, requestedFileData)
+        send_file(connectionSocket, 'HTTP/1.1 200 OK\r\n\r\n', requestedFileData)
+
         print("*** File sent. ***\n")
     except IOError:
         errFile = open('err_page.html', 'r')
         errFileData = errFile.read()
-        send_file(connectionSocket, errFileData)
+        send_file(connectionSocket, 'HTTP/1.1 404 NOT FOUND\r\n\r\n', errFileData)
+
         connectionSocket.close()
+
+serverSocket.close()
