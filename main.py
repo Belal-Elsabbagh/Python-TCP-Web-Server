3c9868399b1b061dafdf6844afa7c55bc2aaa89c
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-06-26 18:22:40 +0200

    Added main function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from socket import *
 
 
+HOST_IP: str = "127.0.0.1"
+TCP_PORT: int = 6789
+BUFFER_SIZE: int = 4096
+
+
 def send_file(conn_socket: socket, http_response: str, data_str: str) -> None:
     """
     Sends a string through the connection socket
@@ -11,7 +16,7 @@ def send_file(conn_socket: socket, http_response: str, data_str: str) -> None:
     """
     conn_socket.send(http_response.encode())
     conn_socket.send(data_str.encode())
-    connectionSocket.send("\r\n".encode('UTF-8'))
+    conn_socket.send("\r\n".encode('UTF-8'))
 
 
 def get_filename_from_http_request(request: str) -> str:
@@ -24,33 +29,34 @@ def get_filename_from_http_request(request: str) -> str:
     return request.split()[1][1:]
 
 
-HOST_IP: str = "127.0.0.1"
-TCP_PORT: int = 6789
-BUFFER_SIZE: int = 4096
+def main():
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind((HOST_IP, TCP_PORT))
+    server_socket.listen(4)
+    print('Server initialized\n')
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind((HOST_IP, TCP_PORT))
-serverSocket.listen(4)
-print('Server initialized\n')
+    while True:
+        print('Ready to serve...')
+        connection_socket, addr = server_socket.accept()
+        print('Connection address:', addr)
+        http_request = connection_socket.recv(BUFFER_SIZE).decode('utf-8')
+        print("HTTP Request: " + http_request)
+        filename = get_filename_from_http_request(http_request)
+        try:
+            requested_file = open(filename, 'r')
+            requested_file_data = requested_file.read()
+            send_file(connection_socket, 'HTTP/1.1 200 OK\r\n\r\n', requested_file_data)
 
-while True:
-    print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()
-    print('Connection address:', addr)
-    http_request = connectionSocket.recv(BUFFER_SIZE).decode('utf-8')
-    print("HTTP Request: " + http_request)
-    filename = get_filename_from_http_request(http_request)
-    try:
-        requestedFile = open(filename, 'r')
-        requestedFileData = requestedFile.read()
-        send_file(connectionSocket, 'HTTP/1.1 200 OK\r\n\r\n', requestedFileData)
+            print("*** File sent. ***\n")
+        except IOError:
+            err_file = open('err_page.html', 'r')
+            err_file_data = err_file.read()
+            send_file(connection_socket, 'HTTP/1.1 404 NOT FOUND\r\n\r\n', err_file_data)
+            connection_socket.close()
 
-        print("*** File sent. ***\n")
-    except IOError:
-        errFile = open('err_page.html', 'r')
-        errFileData = errFile.read()
-        send_file(connectionSocket, 'HTTP/1.1 404 NOT FOUND\r\n\r\n', errFileData)
-        connectionSocket.close()
+    # noinspection PyUnreachableCode
+    server_socket.close()
 
-# noinspection PyUnreachableCode
-serverSocket.close()
+
+if __name__ == "__main__":
+    main()
